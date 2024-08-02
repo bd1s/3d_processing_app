5db965c692f721d9c7d07ad7080eaf1a95b83298
bd1s <essalihibadiaapro@gmail.com>
2024-08-02 23:20:45 +0200

    navbar excluded

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/main/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
 
       <body className={`${inter.className} bg-[#ffffff overflow-y-scroll overflow-x-hidden`}>
-      <Navbar />
+      
         {children}
       </body>
     </html>
